7fb709695383cfde7856fa3472b16b0d2f01020d
ludengg <lianghe@zhiyuandeMacBook-Pro.local>
2015-06-19 11:29:22 +0800

    init

---
+++ fuqitong/fuqitong/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  fuqitong
+//
+//  Created by 志远 何 on 15/6/19.
+//  Copyright (c) 2015年 xiangchuang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ fuqitong/fuqitongTests/fuqitongTests.swift
@@ -0,0 +1,36 @@
+//
+//  fuqitongTests.swift
+//  fuqitongTests
+//
+//  Created by 志远 何 on 15/6/19.
+//  Copyright (c) 2015年 xiangchuang. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class fuqitongTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
